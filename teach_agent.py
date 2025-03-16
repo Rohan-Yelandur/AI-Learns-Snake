31400c95fd0af1e9bd8f1de41a379025908b3214
Rohan-Yelandur <ry7games@gmail.com>
2025-03-16 14:59:35 -0500

    Bug fix

--- teach_agent.py
+++ teach_agent.py
@@ -19,7 +19,7 @@ class Agent:
         self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.model = Linear_QNet(11, 256, 3)
-        self.trainer = QTrainer(self.model, lr=LEARNING_RATE, gamma=self.gamma)
+        self.trainer = QTrainer(self.model, LEARNING_RATE=LEARNING_RATE, gamma=self.gamma)
 
 
     def get_state(self, game):
